1e2b3749d0fcc7ab8a21c8c59747dede48a02dec
thememetsar <81377641+thememetsar@users.noreply.github.com>
2022-12-18 21:59:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def press(button):
         pyautogui.sleep(1)
         pyautogui.typewrite('https://192.168.1.12:6001/RemoteViewSettings')
         pyautogui.press('enter')
+    if button == "Content Manager":
+        pyautogui.hotkey('win')
+        pyautogui.sleep(2)
+        pyautogui.typewrite('content manager')
+        pyautogui.press('enter')
+        pyautogui.sleep(2)
+        pyautogui.press('control'+'v')
 
 app = gui("RRM", "400x200")
 app.startTabbedFrame("MainMenu") 
@@ -31,6 +38,7 @@ update_time()
 app.startTab("ControlPannel")
 app.button("Run Sigma", press)
 app.button("New Race LB", press)
+app.button("Content Manager", press)
 app.stopTab()
 
 
