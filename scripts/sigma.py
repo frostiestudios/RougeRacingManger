19e934c35d25a381403da891ebeab4a47a64f501
Dromo2106 <dromomod@gmail.com>
2022-12-24 15:01:15 -0800

    commit

--- scripts/sigma.py
+++ scripts/sigma.py
@@ -1,14 +1,14 @@
 import pyautogui
 from appJar import gui
 
-def press_windows_button():
-    pyautogui.hotkey('win')   
-    pyautogui.sleep(1)
-    pyautogui.typewrite('sigma simulation')
-    pyautogui.press('enter')
-    pyautogui.sleep(3)
-    pyautogui.press('ctrl','s')
+
+pyautogui.hotkey('win')   
+pyautogui.sleep(1)
+pyautogui.typewrite('sigma simulation')
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.press('ctrl','s')
 
 app = gui()
-app.addButton("Press Windows button", press_windows_button)
+app.addLabel("Sigma Now Running")
 app.go()
