d91998ce97a77febf2bc5866de82a0210d37a099
Dromo2106 <dromomod@gmail.com>
2022-12-24 14:51:26 -0800

    message

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from appJar import gui
 import webbrowser
 import time
 import pyautogui
+import subprocess
+import os
 
 def press(button):
     if button == "OpenWeb":
@@ -16,14 +18,20 @@ def press(button):
         pyautogui.sleep(1)
         pyautogui.typewrite('https://192.168.1.12:6001/RemoteViewSettings')
         pyautogui.press('enter')
-    if button == "Content Manager":
-        pyautogui.hotkey('win')
-        pyautogui.sleep(2)
-        pyautogui.typewrite('content manager')
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-        pyautogui.press('control'+'v')
-
+    if button == "Mute":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "toggle"])
+    elif button == "Lower":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%-"])
+    elif button == "Raise":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%+"])
+def restart(btn):
+    if btn == "Restart":
+        os.system("shutdown /r /t 0")
+    if btn == "Shut Down":
+        os.system("shutdown /s /t 1")
+    if btn == "Sleep":
+        subprocess.run(["rundll32.exe", "powrprof.dll,SetSuspendState"])
+        
 app = gui("RRM", "400x200")
 app.startTabbedFrame("MainMenu") 
 app.startTab("MainMenu")  
@@ -38,7 +46,9 @@ update_time()
 app.startTab("ControlPannel")
 app.button("Run Sigma", press)
 app.button("New Race LB", press)
-app.button("Content Manager", press)
+app.addButtons(["Restart","Shut Down","Sleep"], restart)
+app.addLabel("l1", "Volume Control")
+app.addButtons(["Mute", "Lower", "Raise"], press)
 app.stopTab()
 
 
