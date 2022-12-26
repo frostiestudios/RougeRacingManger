2fd40a388860e57af4bad416ba57cdd1ef4609a7
thememetsar <mattwiner04@gmail.com>
2022-12-25 20:23:48 -0800

    12-25 IV

--- command.py
+++ command.py
@@ -1,78 +1,46 @@
 import socket
 from appJar import gui
-import ctypes
-import os
 
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a host and port
+server_socket.bind(('localhost', 8000))
+
+# Start listening for incoming connections
+server_socket.listen()
+
+# Accept an incoming connection
+client_socket, client_address = server_socket.accept()
+
+# Create a GUI app using appjar
 app = gui()
+# Add a button to send a message to the client
+def send_message(btn):
+    message = app.getEntry("Message")
+    client_socket.send(message.encode())
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+app.addButton("Send message", send_message)
 
-# Bind the socket to the port
-server_address = ('192.168.0.79', 10000)
-print('Starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-def restart(btn):
-    # Restart the computer
-    pass
-
-def sleep(btn):
-    # Put the computer to sleep
-    pass
-
-def shutdown(btn):
-    # Shut down the computer
-    pass
-def select_file(btn):
-    # Open a file dialog to select a file
+# Add a button to send a file to the client
+def send_file(btn):
     file_path = app.openBox()
-    # Get the file name and size
-    file_name = os.path.basename(file_path)
-    file_size = os.path.getsize(file_path)
-    # Update the GUI with the file name and size
-    app.setLabel('File Name', file_name)
-    app.setLabel('File Size', str(file_size))
-    
-# Create the GUI
-app.addButtons(['Restart', 'Sleep', 'Shutdown'], [restart, sleep, shutdown])
-app.addLabel('Select a file to send:')
-app.addButton('Select File', select_file)
-app.addLabel('File Name:')
-app.addLabel('File Size:')
-app.addLabel('Connected Clients:')
-app.addLabel('IP Addresses:')
+    with open(file_path, 'rb') as file:
+        client_socket.sendall(file)
+
+app.addButton("Send file", send_file)
+
+# Add a button to send a sleep command to the client
+def send_sleep_command(btn):
+    client_socket.send("sleep".encode())
+
+app.addButton("Send sleep command", send_sleep_command)
+
+# Add a button to send a reset command to the client
+def send_reset_command(btn):
+    client_socket.send("reset".encode())
+
+app.addButton("Send reset command", send_reset_command)
+
+# Run the GUI
 app.go()
-
-while True:
-    # Wait for a connection
-    print('Waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('Connection from', client_address)
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            if data:
-                command = data.decode()
-                # Execute the command here
-                print('Received "{}"'.format(command))
-                if command == 'restart':
-                    restart(None)
-                elif command == 'sleep':
-                    sleep(None)
-                elif command == 'shutdown':
-                    shutdown(None)
-                
-            else:
-                print('No more data from', client_address)
-                break
-            
-            
-    finally:
-        # Clean up the connection
-        connection.close()
