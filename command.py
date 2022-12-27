9f98a1b4335f6fe3fee0ee99bbc420605126d5cc
thememetsar <81377641+thememetsar@users.noreply.github.com>
2022-12-26 22:08:14 -0800

    26-III

--- command.py
+++ command.py
@@ -1,46 +1,98 @@
-import socket
 from appJar import gui
+import webbrowser
+import time
+import pyautogui
+import subprocess
+import os
+import io
 
-# Create a socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to a host and port
-server_socket.bind(('localhost', 8000))
+def press(button):
+    if button == "OpenWeb":
+        app.open_web("http://www.google.com")
+    if button == "Run Sigma":
+        pyautogui.hotkey('win')
+        pyautogui.sleep(1)
+        pyautogui.typewrite('sigma simulation')
+        pyautogui.press('enter')
+    if button == "Content Manager":
+        pyautogui.sleep(1)
+        pyautogui.hotkey('win')
+        pyautogui.sleep(3)
+        pyautogui.typewrite('content manager')
+        pyautogui.sleep(2)
+        pyautogui.press('enter')
+    if button == "Mute":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "toggle"])
+    elif button == "Lower":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%-"])
+    elif button == "Raise":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%+"])
+def power(btn):
+    if btn == "Restart":
+        os.system("shutdown /r /t 0")
+    if btn == "Shut Down":
+        os.system("shutdown /s /t 1")
+    if btn == "Sleep":
+        subprocess.run(["rundll32.exe", "powrprof.dll,SetSuspendState"])
+        
+def client(btn):
+    if btn == "ADD":
+        cnm = app.getEntry("Client Name")
+        cip = app.getEntry("IP")
+        with open ( "clients.cfg","a") as outFile:
+            outFile.write("client name: ")
+            outFile.write(cnm)
+            outFile.write("\n")
+            outFile.write("ip adress  : ")
+            outFile.write(cip)
+            outFile.write("\n")
+            outFile.write("------ \n")
+            outFile.close()
+        print(cnm,cip)
+        
+        
+def settings(btn):
+    if btn == "View Clients File":
+        pyautogui.sleep(1)
+        pyautogui.hotkey('win')
 
-# Start listening for incoming connections
-server_socket.listen()
 
-# Accept an incoming connection
-client_socket, client_address = server_socket.accept()
+app = gui("Command")
+app.startTabbedFrame("MainMenu") 
 
-# Create a GUI app using appjar
-app = gui()
-# Add a button to send a message to the client
-def send_message(btn):
-    message = app.getEntry("Message")
-    client_socket.send(message.encode())
+app.startTab("MainMenu")  
 
-app.addButton("Send message", send_message)
 
-# Add a button to send a file to the client
-def send_file(btn):
-    file_path = app.openBox()
-    with open(file_path, 'rb') as file:
-        client_socket.sendall(file)
+app.startTab("Local Commands")
+app.addLabel("l1","Standard Controls")
+app.addButtons(["Run Sigma","Content Manager"], press)
+app.addLabel("l2","Power Controls")
+app.addButtons(["Restart","Shut Down","Sleep"], power)
+app.addLabel("l3", "Volume Control")
+app.addButtons(["Mute", "Lower", "Raise"], press)
+app.stopTab()
 
-app.addButton("Send file", send_file)
+app.startTab("Clients")
+app.addListBox("clients_list")
+with open("clients.cfg") as clients_file:
+    for line in clients_file.readlines():
+        if line.startswith("Client Name:"):
+            client_name = line[12:].strip()
+            app.addListItem("clients_list", client_name)
+app.addButton("Add New", client)
+app.stopTab()
 
-# Add a button to send a sleep command to the client
-def send_sleep_command(btn):
-    client_socket.send("sleep".encode())
+app.startTab("New Client")
+app.addLabelEntry("Client Name")
+app.addLabelEntry("IP")
+app.addButtons(["Clear","ADD"],client)
+app.stopTab()
 
-app.addButton("Send sleep command", send_sleep_command)
+app.startTab("settings")
+app.startFrame("Client Settings")
+app.addButtons(["View Clients File"], settings)
+app.stopFrame()
+app.stopTab()
 
-# Add a button to send a reset command to the client
-def send_reset_command(btn):
-    client_socket.send("reset".encode())
-
-app.addButton("Send reset command", send_reset_command)
-
-# Run the GUI
 app.go()
--- conquer.py
+++ conquer.py
@@ -1,16 +1,6 @@
 import socket
 from appJar import gui
 
-# Read the server address from the configuration file
-with open('config.txt') as config_file:
-    server_address = config_file.readline().strip()
-
-# Create a socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to the server
-client_socket.connect((server_address, 8000))
-
 # Create a GUI app using appjar
 app = gui()
 
@@ -21,12 +11,5 @@ app.addLabel("Messages")
 app.go()
 
 # Keep receiving data from the server
-while True:
-    data = client_socket.recv(1024).decode()
-
-    # If the received data is a message, display it
-    if data.startswith("message:"):
-        message = data[8:]
-        app.setLabel("Messages", message)
 
     # If the received data is a file
